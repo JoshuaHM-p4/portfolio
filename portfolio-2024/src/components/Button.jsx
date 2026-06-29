bb2fe1967af4977b9b7892a5f376670e6243b6ae
JoshuaHM-p4 <joshuahm.2004@gmail.com>
2026-06-29 17:02:58 +0800

    style: replace outer shadows with shadow-inner on all buttons

--- portfolio-2024/src/components/Button.jsx
+++ portfolio-2024/src/components/Button.jsx
@@ -1,9 +1,9 @@
-const Button = ({ text, onClick, className = "", active=false}) => {
+const Button = ({ text, onClick, className = "", active = false }) => {
   const hasRounded = className.includes('rounded');
   return (
     <button
-      className={`border border-white-100 shadow-md transition-all duration-500 ease-[cubic-bezier(0.16,1,0.3,1)] px-4 py-2 mt-5
-      text-black-400 font-normal backdrop-blur-sm bg-white-500/10 hover:bg-white-500/60 hover:shadow-lg water-fill-btn
+      className={`border border-white-100 shadow-inner transition-all duration-500 ease-[cubic-bezier(0.16,1,0.3,1)] px-4 py-2 mt-5
+      text-black-400 font-normal backdrop-blur-sm bg-white-500/10 hover:bg-white-500/60 water-fill-btn
       ${hasRounded ? '' : 'md:rounded-2xl rounded-full'} 
       ${active ? "is-active !text-white-100 !border-transparent !ring-0" : ""}
       ${className}
